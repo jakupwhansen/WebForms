0a57e5b1fb23cc6c562b12fb6b14f10262d65c53
jakupwhansen <jakupwhansen@hotmail.com>
2022-12-08 09:11:00 +0100

    Add project files.

---
+++ WebFormsSiteForGitFramework472/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsSiteForGitFramework472.Startup))]
+namespace WebFormsSiteForGitFramework472
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
